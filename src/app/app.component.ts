74a4d6832c8b6474b579d2701d428438e7c964ad
Jattesh-Hittinahalli <autoexplain@gmail.com>
2020-11-09 11:41:33 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,75 +20,59 @@ export class AppComponent {
 
   constructor(private user: LoginService) {
     this.user.getData().subscribe(data => {
-      debugger;
-      var resultSet: {
-        id: number;
-        email: string;
-        first_name: string;
-        last_name: string;
-        avatar: string;
-      }[];
-      // data["data"].foreach(readValues());
-
-      for (var i = 1; i <= data["data"].length; i++) {
-        resultSet[i].id = data["data"][i].id;
-        resultSet[i].first_name = data["data"][i].first_name;
-        resultSet[i].last_name = data["data"][i].last_name;
-        resultSet[i].avatar = data["data"][i].avatar;
-      }
-      console.log(resultSet);
-      //this.data = data;
+      console.warn(data);
+      this.data = data;
     });
 
-    // this.model = [
-    //   {
-    //     id: 7,
-    //     email: "michael.lawson@reqres.in",
-    //     first_name: "Michael",
-    //     last_name: "Lawson",
-    //     avatar:
-    //       "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
-    //   },
-    //   {
-    //     id: 8,
-    //     email: "lindsay.ferguson@reqres.in",
-    //     first_name: "Lindsay",
-    //     last_name: "Ferguson",
-    //     avatar:
-    //       "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
-    //   },
-    //   {
-    //     id: 9,
-    //     email: "tobias.funke@reqres.in",
-    //     first_name: "Tobias",
-    //     last_name: "Funke",
-    //     avatar:
-    //       "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
-    //   },
-    //   {
-    //     id: 10,
-    //     email: "byron.fields@reqres.in",
-    //     first_name: "Byron",
-    //     last_name: "Fields",
-    //     avatar:
-    //       "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
-    //   },
-    //   {
-    //     id: 11,
-    //     email: "george.edwards@reqres.in",
-    //     first_name: "George",
-    //     last_name: "Edwards",
-    //     avatar:
-    //       "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
-    //   },
-    //   {
-    //     id: 12,
-    //     email: "rachel.howell@reqres.in",
-    //     first_name: "Rachel",
-    //     last_name: "Howell",
-    //     avatar:
-    //       "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
-    //   }
-    // ];
+    this.model = [
+      {
+        id: 7,
+        email: "michael.lawson@reqres.in",
+        first_name: "Michael",
+        last_name: "Lawson",
+        avatar:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
+      },
+      {
+        id: 8,
+        email: "lindsay.ferguson@reqres.in",
+        first_name: "Lindsay",
+        last_name: "Ferguson",
+        avatar:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
+      },
+      {
+        id: 9,
+        email: "tobias.funke@reqres.in",
+        first_name: "Tobias",
+        last_name: "Funke",
+        avatar:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
+      },
+      {
+        id: 10,
+        email: "byron.fields@reqres.in",
+        first_name: "Byron",
+        last_name: "Fields",
+        avatar:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/russoedu/128.jpg"
+      },
+      {
+        id: 11,
+        email: "george.edwards@reqres.in",
+        first_name: "George",
+        last_name: "Edwards",
+        avatar:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/mrmoiree/128.jpg"
+      },
+      {
+        id: 12,
+        email: "rachel.howell@reqres.in",
+        first_name: "Rachel",
+        last_name: "Howell",
+        avatar:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/hebertialmeida/128.jpg"
+      }
+    ];
   }
 }
